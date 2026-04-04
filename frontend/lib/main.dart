b4d7cc4e45692ae1a5170925f94b9e0ad397b9ec
Pablo Andura <pablo.andura@gmail.com>
2026-04-04 11:03:38 -0300

    Connect Flutter app to Firebase project news-lab-2ba2d

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_lab/config/routes/routes.dart';
 import 'package:news_lab/config/theme/app_themes.dart';
+import 'package:news_lab/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:news_lab/features/auth/presentation/bloc/auth_event.dart';
 import 'package:news_lab/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
 import 'package:news_lab/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
 import 'package:news_lab/features/daily_news/presentation/pages/home/daily_news.dart';
@@ -10,7 +13,7 @@ import 'package:news_lab/injection_container.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await initializeDependencies();
   runApp(const NewsLabApp());
 }
@@ -20,8 +23,15 @@ class NewsLabApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<RemoteArticlesBloc>(
-      create: (_) => sl<RemoteArticlesBloc>()..add(const GetArticles()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (_) => sl<AuthBloc>()..add(const CheckAuthStatus()),
+        ),
+        BlocProvider<RemoteArticlesBloc>(
+          create: (_) => sl<RemoteArticlesBloc>()..add(const GetArticles()),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'News Lab',
